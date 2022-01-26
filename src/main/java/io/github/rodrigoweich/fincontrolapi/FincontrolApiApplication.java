1d23ac1fc8637ae44ba5402c3e3cf85332a0272f
rodrigoweich <rodrigoweich@hotmail.com>
2022-01-25 22:04:26 -0300

    First commit

---
+++ src/main/java/io/github/rodrigoweich/fincontrolapi/FincontrolApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rodrigoweich.fincontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincontrolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rodrigoweich/fincontrolapi/FincontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rodrigoweich.fincontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
